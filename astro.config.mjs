ad8c2c59962627ad64ebfdb87478e1a0f2f3a841
Pavel Gorlov <i@pavliko.com>
2025-07-01 22:23:26 +0300

    feat(cv): add cv page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [astroI18next()]
+    integrations: [astroI18next()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
